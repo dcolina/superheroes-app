8915805d2c2e982aca00aff72ab653d0c60d4c6b
Daniel Colina <daniel.colina@dotcms.com>
2025-06-04 20:55:17 +0200

    #superheroes Final prototype for landing page.

--- frontend/src/app/features/home/pages/home/home.ts
+++ frontend/src/app/features/home/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../../../shared/components/header/header';
-import { FooterNav } from '../../../../shared/components/footer-nav/footer-nav';
 import { SuperheroCarousel } from '../../../superheroes/components/superhero-carousel/superhero-carousel';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, FooterNav, SuperheroCarousel],
+  standalone: true,
+  imports: [Header, SuperheroCarousel],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
